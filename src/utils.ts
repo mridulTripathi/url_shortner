298b1e2c8091ea09bb2928d72cf0c7738f108f30
Mridul Tripathi <mridulmtripathi@gmail.com>
2020-09-19 11:43:45 +0530

    added methods for creating and reversing urls

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,34 @@
 const SHORT_URL_CHARACTER_SET =
   "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-export function getShortURL(longUrl: string) {}
+export function getShortURL(urlID: number) {
+  var shortURL = "";
+
+  while (urlID > 0) {
+    shortURL =
+      SHORT_URL_CHARACTER_SET.charAt(urlID % SHORT_URL_CHARACTER_SET.length) +
+      shortURL;
+    urlID = Math.floor(urlID / SHORT_URL_CHARACTER_SET.length);
+  }
+
+  return shortURL;
+}
+
+export function getUrlID(shortURL: string) {
+  var urlId = 0;
+  var power = 0;
+
+  while (shortURL.length > 0) {
+    var charValue = shortURL.slice(-1);
+    shortURL = shortURL.slice(0, -1);
+
+    urlId =
+      urlId +
+      SHORT_URL_CHARACTER_SET.indexOf(charValue) *
+        Math.pow(SHORT_URL_CHARACTER_SET.length, power);
+
+    power++;
+  }
+
+  return urlId;
+}
